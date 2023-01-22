888457f6e401e55a58d505c162abb07ca93f1689
4l43 <103286987+4l43@users.noreply.github.com>
2023-01-22 21:23:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-const cursor = document.querySelector('.cursor');
+const cursor= document.querySelector('cursor');
 
-document.addEventListener('mousemove', e => {
-    cursor.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
-})
-
-document.addEventListener('click', ()=>{
-    cursor.classList.add('expand');
-
-    setTimeout(()=>{
-        cursor.classList.remove("expand");
-    }, 500);
+document.addEventListener("mousemove", (e)=> {
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+ 
 })
